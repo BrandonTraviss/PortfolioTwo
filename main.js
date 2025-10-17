f770014a0673b8469bc754eec7cce2a476ea1260
BrandonTraviss <btraviss1987@gmail.com>
2025-10-17 13:08:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const navSvg = document.getElementById("nav-svg");
+const navMenu = document.getElementById("mobile-nav");
+
+let clicked = false;
+
+function toggleMenu() {
+    clicked = !clicked;
+    console.log("Menu clicked:", clicked);
+    if(clicked){
+        navMenu.classList.remove("inactive")
+        navMenu.classList.add("active")
+        navSvg.src="./img/svg/close.svg"
+    }
+    if(!clicked){
+        navMenu.classList.remove("active")
+        navMenu.classList.add("inactive")
+        navSvg.src="./img/svg/hamburger.svg"
+    }
+}
+
+// Attach the click listener
+navSvg.addEventListener("click", toggleMenu);
